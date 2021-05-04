7ec68b7a00b2b40abb7001f2884655355f9878ca
JosephCoiffman <67018154+JosephCoiffman@users.noreply.github.com>
2021-05-04 17:03:05 -0400

    added filter
    added filter and created filter functions for each of them.

---
+++ script.js
@@ -0,0 +1,41 @@
+  function filterTheTeamsTable(){
+	 let filterBy = document.getElementById("team-filter").value;
+  //code to SELECT table with that specific value
+  console.log("this worked!, " + filterBy);
+}
+
+  function filterTheSeasonsTable(){
+	 let filterBy = document.getElementById("season-filter").value;
+  //code to SELECT table with that specific value
+  console.log("this worked!, " + filterBy);
+}
+
+  function filterThePLayersTable(){
+	 let filterBy = document.getElementById("player-filter").value;
+  //code to SELECT table with that specific value
+  console.log("this worked!, " + filterBy);
+}
+
+  function filterThePtrsTable(){
+	 let filterBy = document.getElementById("ptr-filter").value;
+  //code to SELECT table with that specific value
+  console.log("this worked!, " + filterBy);
+}
+
+ function filterTheLhTable(){
+	 let filterBy = document.getElementById("league-histories-filter").value;
+  //code to SELECT table with that specific value
+  console.log("this worked!, " + filterBy);
+}
+
+ function filterTheCoachesTable(){
+	 let filterBy = document.getElementById("coach-filter").value;
+  //code to SELECT table with that specific value
+  console.log("this worked!, " + filterBy);
+}
+
+ function filterTheCtrTable(){
+	 let filterBy = document.getElementById("ctr-filter").value;
+  //code to SELECT table with that specific value
+  console.log("this worked!, " + filterBy);
+}
