29f1fb6aed6c264a47ac814b1c89f67ebbc37398
Mark Philip Bautista <markpmbautista@gmail.com>
2021-05-12 19:22:52 -0700

    Update project_dml.sql

--- project_dml.sql
+++ project_dml.sql
@@ -8,7 +8,6 @@ INSERT INTO league_histories ()
 VALUES ();
 
 -- League Histories Page update query
-
 UPDATE league_histories
 SET league_name = $leagueName, completed_seasons_num = $completedSeasonsNum
 WHERE league_histories_id = $id;
@@ -26,6 +25,11 @@ INSERT INTO seasons ()
 VALUES ();
 
 -- Seasons Page update query
+UPDATE seasons
+SET league_id = $leagueID, season_name = $seasonName, date_season_started = $dateSeasonStarted, 
+date_season_ended = $dateSeasonEnded, league_champion = $leagueChampion, western_champion = $westernChampion, 
+eastern_champion = $easternChampion, most_valuable_player = $mostValuablePlayer, sixth_man_of_the_year = $sixthManOfTheYear
+WHERE seasons_id = $id;
 
 -- Seasons Page delete query
 DELETE FROM seasons WHERE season_id = $id;
@@ -40,6 +44,15 @@ INSERT INTO teams ()
 VALUES ();
 
 -- Teams Page update query
+UPDATE teams
+SET season_id = $seasonID, team_name = $teamName, team_place_city = $teamPlaceCity, 
+team_place_state = $teamPlaceState, points_per_game = $pointsPerGame, assists_per_game = $assistsPerGame, 
+steals_per_game = $stealsPerGame, blocks_per_game = $blocksPerGame, rebounds_per_game = $reboundsPerGame, 
+fouls_per_game = $foulsPerGame, fg_attempted_per_game = $fgAttemptedPerGame, 3pt_attempted_per_game = $3ptAttemptedPerGame, 
+fg_percentage = $fgPercentage, 3pt_fg_percentage = $3ptfgPercentage, ft_percentage = $ftPercentage, 
+point_differential = $pointDifferential, reg_season_wins = $regSeasonWins, reg_season_losses = $regSeasonLosses, 
+reg_season_win_percentage = $regSeasonWinPercentage
+WHERE teams_id = $id;
 
 -- Teams Page delete query
 DELETE FROM teams WHERE team_id = $id;
@@ -54,6 +67,9 @@ INSERT INTO players ()
 VALUES ();
 
 -- Players Page update query
+UPDATE players
+SET player_name = $playerName, player_jersey_number = $playerJerseyNumber
+WHERE players_id = $id;
 
 -- Players Page delete query
 DELETE FROM players WHERE player_id = $id;
@@ -68,6 +84,9 @@ INSERT INTO player_team_relations ()
 VALUES ();
 
 -- Player Team Relations Page update query
+UPDATE player_team_relations
+SET player_id = $playerID, team_id = $teamID
+WHERE player_team_relations_id = $id;
 
 -- Player Team Relations Page delete query
 DELETE FROM player_team_relations WHERE player_team_relations_id = $id;
@@ -82,6 +101,9 @@ INSERT INTO coaches ()
 VALUES ();
 
 -- Coaches Page update query
+UPDATE coaches
+SET coach_name = $coachName, coach_wins = $coachWins, coach_losses = $coachLosses, coach_win_percentage = $coachWinPercentage
+WHERE coach_id = $id;
 
 -- Coaches Page delete query
 DELETE FROM coaches WHERE coach_id = $id;
@@ -96,6 +118,9 @@ INSERT INTO coach_team_relations ()
 VALUES ();
 
 -- Coach Team Relations Page update query
+UPDATE coach_team_relations
+SET coach_id = $coachID, team_id = $teamID
+WHERE coach_team_relations_id = $id;
 
 -- Coach Team Relations Page delete query
 DELETE FROM coach_team_relations WHERE coach_team_relations_id = $id;
